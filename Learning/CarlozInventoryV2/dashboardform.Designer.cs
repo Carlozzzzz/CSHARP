ba1ecaf640def34c35dc04d501c2c16792f6a937
Carloz <112228701+Carlozzzzz@users.noreply.github.com>
2025-08-04 13:26:25 +0800

    carloz inventory : update dashboard and settings design

--- Learning/CarlozInventoryV2/dashboardform.Designer.cs
+++ Learning/CarlozInventoryV2/dashboardform.Designer.cs
@@ -28,34 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblAdmin = new System.Windows.Forms.Label();
+            this.panelProducts = new System.Windows.Forms.Panel();
+            this.panelLblProducts = new System.Windows.Forms.Panel();
+            this.lblPruducts = new System.Windows.Forms.Label();
+            this.lblProductsCount = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblSupplier = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblSupplierCount = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblUsers = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblUsersCount = new System.Windows.Forms.Label();
+            this.panelOtherDesc = new System.Windows.Forms.Panel();
+            this.panelHeader.SuspendLayout();
+            this.panelProducts.SuspendLayout();
+            this.panelLblProducts.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panelHeader
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(141, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "dashboard";
+            this.panelHeader.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelHeader.Controls.Add(this.lblAdmin);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.ForeColor = System.Drawing.Color.White;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(800, 60);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblAdmin
+            // 
+            this.lblAdmin.AutoSize = true;
+            this.lblAdmin.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdmin.Location = new System.Drawing.Point(13, 15);
+            this.lblAdmin.Name = "lblAdmin";
+            this.lblAdmin.Size = new System.Drawing.Size(109, 31);
+            this.lblAdmin.TabIndex = 1;
+            this.lblAdmin.Text = "Admin : ";
+            // 
+            // panelProducts
+            // 
+            this.panelProducts.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelProducts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelProducts.Controls.Add(this.lblProductsCount);
+            this.panelProducts.Location = new System.Drawing.Point(29, 80);
+            this.panelProducts.Name = "panelProducts";
+            this.panelProducts.Size = new System.Drawing.Size(203, 175);
+            this.panelProducts.TabIndex = 1;
+            // 
+            // panelLblProducts
+            // 
+            this.panelLblProducts.BackColor = System.Drawing.Color.White;
+            this.panelLblProducts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLblProducts.Controls.Add(this.lblPruducts);
+            this.panelLblProducts.Location = new System.Drawing.Point(29, 255);
+            this.panelLblProducts.Name = "panelLblProducts";
+            this.panelLblProducts.Size = new System.Drawing.Size(203, 35);
+            this.panelLblProducts.TabIndex = 2;
+            // 
+            // lblPruducts
+            // 
+            this.lblPruducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPruducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPruducts.Location = new System.Drawing.Point(0, 0);
+            this.lblPruducts.Name = "lblPruducts";
+            this.lblPruducts.Size = new System.Drawing.Size(201, 33);
+            this.lblPruducts.TabIndex = 0;
+            this.lblPruducts.Text = "Products";
+            this.lblPruducts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblProductsCount
+            // 
+            this.lblProductsCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblProductsCount.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductsCount.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblProductsCount.Location = new System.Drawing.Point(0, 0);
+            this.lblProductsCount.Name = "lblProductsCount";
+            this.lblProductsCount.Size = new System.Drawing.Size(201, 173);
+            this.lblProductsCount.TabIndex = 5;
+            this.lblProductsCount.Text = "0";
+            this.lblProductsCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblSupplier);
+            this.panel2.Location = new System.Drawing.Point(295, 255);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(203, 35);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblSupplier
+            // 
+            this.lblSupplier.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSupplier.Location = new System.Drawing.Point(0, 0);
+            this.lblSupplier.Name = "lblSupplier";
+            this.lblSupplier.Size = new System.Drawing.Size(201, 33);
+            this.lblSupplier.TabIndex = 0;
+            this.lblSupplier.Text = "Supplier";
+            this.lblSupplier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.lblSupplierCount);
+            this.panel3.Location = new System.Drawing.Point(295, 80);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(203, 175);
+            this.panel3.TabIndex = 3;
+            // 
+            // lblSupplierCount
+            // 
+            this.lblSupplierCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSupplierCount.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSupplierCount.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSupplierCount.Location = new System.Drawing.Point(0, 0);
+            this.lblSupplierCount.Name = "lblSupplierCount";
+            this.lblSupplierCount.Size = new System.Drawing.Size(201, 173);
+            this.lblSupplierCount.TabIndex = 5;
+            this.lblSupplierCount.Text = "0";
+            this.lblSupplierCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.lblUsers);
+            this.panel4.Location = new System.Drawing.Point(563, 255);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(203, 35);
+            this.panel4.TabIndex = 7;
+            // 
+            // lblUsers
+            // 
+            this.lblUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsers.Location = new System.Drawing.Point(0, 0);
+            this.lblUsers.Name = "lblUsers";
+            this.lblUsers.Size = new System.Drawing.Size(201, 33);
+            this.lblUsers.TabIndex = 0;
+            this.lblUsers.Text = "Users";
+            this.lblUsers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.lblUsersCount);
+            this.panel5.Location = new System.Drawing.Point(563, 80);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(203, 175);
+            this.panel5.TabIndex = 6;
+            // 
+            // lblUsersCount
+            // 
+            this.lblUsersCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblUsersCount.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsersCount.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblUsersCount.Location = new System.Drawing.Point(0, 0);
+            this.lblUsersCount.Name = "lblUsersCount";
+            this.lblUsersCount.Size = new System.Drawing.Size(201, 173);
+            this.lblUsersCount.TabIndex = 5;
+            this.lblUsersCount.Text = "0";
+            this.lblUsersCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelOtherDesc
+            // 
+            this.panelOtherDesc.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panelOtherDesc.Location = new System.Drawing.Point(29, 320);
+            this.panelOtherDesc.Name = "panelOtherDesc";
+            this.panelOtherDesc.Size = new System.Drawing.Size(738, 230);
+            this.panelOtherDesc.TabIndex = 8;
             // 
             // dashboardform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(800, 570);
+            this.Controls.Add(this.panelOtherDesc);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panelLblProducts);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panelProducts);
+            this.Controls.Add(this.panelHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "dashboardform";
             this.Text = "dashboardform";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelProducts.ResumeLayout(false);
+            this.panelLblProducts.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblAdmin;
+        private System.Windows.Forms.Panel panelProducts;
+        private System.Windows.Forms.Panel panelLblProducts;
+        private System.Windows.Forms.Label lblPruducts;
+        private System.Windows.Forms.Label lblProductsCount;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblSupplier;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblSupplierCount;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblUsers;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblUsersCount;
+        private System.Windows.Forms.Panel panelOtherDesc;
     }
 }
--- Learning/CarlozInventoryV2/settingsform.Designer.cs
+++ Learning/CarlozInventoryV2/settingsform.Designer.cs
@@ -28,34 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panelDarkMode = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.ckbClassicMode = new System.Windows.Forms.CheckBox();
+            this.panelClassicMode = new System.Windows.Forms.Panel();
+            this.lblClassicMode = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.ckbDarkMode = new System.Windows.Forms.CheckBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblDarkMode = new System.Windows.Forms.Label();
+            this.panelLightMode = new System.Windows.Forms.Panel();
+            this.lblLightMode = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.ckbLightMode = new System.Windows.Forms.CheckBox();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panelDarkMode.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panelClassicMode.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panelLightMode.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panelHeader.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panelDarkMode
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(262, 131);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "settingsform";
+            this.panelDarkMode.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelDarkMode.Controls.Add(this.panel7);
+            this.panelDarkMode.Controls.Add(this.panelClassicMode);
+            this.panelDarkMode.Controls.Add(this.panel6);
+            this.panelDarkMode.Controls.Add(this.panel4);
+            this.panelDarkMode.Controls.Add(this.panelLightMode);
+            this.panelDarkMode.Controls.Add(this.panelHeader);
+            this.panelDarkMode.Location = new System.Drawing.Point(12, 12);
+            this.panelDarkMode.Name = "panelDarkMode";
+            this.panelDarkMode.Size = new System.Drawing.Size(776, 546);
+            this.panelDarkMode.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel7.Controls.Add(this.ckbClassicMode);
+            this.panel7.ForeColor = System.Drawing.Color.Transparent;
+            this.panel7.Location = new System.Drawing.Point(505, 117);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(203, 35);
+            this.panel7.TabIndex = 5;
+            // 
+            // ckbClassicMode
+            // 
+            this.ckbClassicMode.AutoSize = true;
+            this.ckbClassicMode.Location = new System.Drawing.Point(11, 10);
+            this.ckbClassicMode.Name = "ckbClassicMode";
+            this.ckbClassicMode.Size = new System.Drawing.Size(89, 17);
+            this.ckbClassicMode.TabIndex = 1;
+            this.ckbClassicMode.Text = "Classic Mode";
+            this.ckbClassicMode.UseVisualStyleBackColor = true;
+            // 
+            // panelClassicMode
+            // 
+            this.panelClassicMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelClassicMode.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelClassicMode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelClassicMode.Controls.Add(this.lblClassicMode);
+            this.panelClassicMode.ForeColor = System.Drawing.Color.White;
+            this.panelClassicMode.Location = new System.Drawing.Point(505, 117);
+            this.panelClassicMode.Name = "panelClassicMode";
+            this.panelClassicMode.Size = new System.Drawing.Size(203, 235);
+            this.panelClassicMode.TabIndex = 4;
+            // 
+            // lblClassicMode
+            // 
+            this.lblClassicMode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblClassicMode.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClassicMode.Location = new System.Drawing.Point(25, 70);
+            this.lblClassicMode.Name = "lblClassicMode";
+            this.lblClassicMode.Size = new System.Drawing.Size(149, 84);
+            this.lblClassicMode.TabIndex = 0;
+            this.lblClassicMode.Text = "Classic Mode";
+            this.lblClassicMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Black;
+            this.panel6.Controls.Add(this.ckbDarkMode);
+            this.panel6.ForeColor = System.Drawing.Color.Transparent;
+            this.panel6.Location = new System.Drawing.Point(285, 117);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(203, 35);
+            this.panel6.TabIndex = 3;
+            // 
+            // ckbDarkMode
+            // 
+            this.ckbDarkMode.AutoSize = true;
+            this.ckbDarkMode.Location = new System.Drawing.Point(11, 10);
+            this.ckbDarkMode.Name = "ckbDarkMode";
+            this.ckbDarkMode.Size = new System.Drawing.Size(79, 17);
+            this.ckbDarkMode.TabIndex = 1;
+            this.ckbDarkMode.Text = "Dark Mode";
+            this.ckbDarkMode.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel4.Controls.Add(this.lblDarkMode);
+            this.panel4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel4.Location = new System.Drawing.Point(285, 117);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(203, 235);
+            this.panel4.TabIndex = 2;
+            // 
+            // lblDarkMode
+            // 
+            this.lblDarkMode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDarkMode.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDarkMode.Location = new System.Drawing.Point(25, 70);
+            this.lblDarkMode.Name = "lblDarkMode";
+            this.lblDarkMode.Size = new System.Drawing.Size(151, 86);
+            this.lblDarkMode.TabIndex = 0;
+            this.lblDarkMode.Text = "Dark mode";
+            this.lblDarkMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelLightMode
+            // 
+            this.panelLightMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelLightMode.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelLightMode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLightMode.Controls.Add(this.lblLightMode);
+            this.panelLightMode.Controls.Add(this.panel5);
+            this.panelLightMode.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelLightMode.Location = new System.Drawing.Point(62, 117);
+            this.panelLightMode.Name = "panelLightMode";
+            this.panelLightMode.Size = new System.Drawing.Size(203, 235);
+            this.panelLightMode.TabIndex = 1;
+            // 
+            // lblLightMode
+            // 
+            this.lblLightMode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLightMode.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLightMode.Location = new System.Drawing.Point(25, 70);
+            this.lblLightMode.Name = "lblLightMode";
+            this.lblLightMode.Size = new System.Drawing.Size(149, 84);
+            this.lblLightMode.TabIndex = 0;
+            this.lblLightMode.Text = "Light Mode";
+            this.lblLightMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DarkGray;
+            this.panel5.Controls.Add(this.ckbLightMode);
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(203, 35);
+            this.panel5.TabIndex = 2;
+            // 
+            // ckbLightMode
+            // 
+            this.ckbLightMode.AutoSize = true;
+            this.ckbLightMode.Location = new System.Drawing.Point(11, 10);
+            this.ckbLightMode.Name = "ckbLightMode";
+            this.ckbLightMode.Size = new System.Drawing.Size(79, 17);
+            this.ckbLightMode.TabIndex = 1;
+            this.ckbLightMode.Text = "Light Mode";
+            this.ckbLightMode.UseVisualStyleBackColor = true;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelHeader.Controls.Add(this.lblHeader);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(776, 47);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblHeader.Location = new System.Drawing.Point(10, 9);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(177, 28);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "Theme Settings";
             // 
             // settingsform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(800, 570);
+            this.Controls.Add(this.panelDarkMode);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "settingsform";
             this.Text = "settingsform";
+            this.panelDarkMode.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panelClassicMode.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panelLightMode.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelDarkMode;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel panelLightMode;
+        private System.Windows.Forms.Label lblLightMode;
+        private System.Windows.Forms.CheckBox ckbLightMode;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.CheckBox ckbDarkMode;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblDarkMode;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.CheckBox ckbClassicMode;
+        private System.Windows.Forms.Panel panelClassicMode;
+        private System.Windows.Forms.Label lblClassicMode;
     }
 }
